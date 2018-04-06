204a918aeec43446d0bd58760cbf4f9d1a5e467e
AlexChronos1806 <alex.afa1806@gmail.com>
2018-04-05 21:32:40 -0300

    Ajustes na chamada do controller

--- src/main/java/br/com/centrologistico/api/controller/DeliveryController.java
+++ src/main/java/br/com/centrologistico/api/controller/DeliveryController.java
@@ -1,10 +1,12 @@
 package br.com.centrologistico.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,9 +27,10 @@ public class DeliveryController {
 	 * @return
 	 */
 	@PostMapping
-	public ResponseEntity<Void> createDelivery(Delivery delivery) {
-		deliveryService.save(delivery);
-		return ResponseEntity.noContent().build();
+	public ResponseEntity<Delivery> createDelivery(@RequestBody Delivery delivery) {
+		Delivery deliverySaved = deliveryService.save(delivery);
+		ResponseEntity<Delivery> entity = new ResponseEntity<Delivery>(deliverySaved, HttpStatus.CREATED);
+		return entity;
 	}
 	
 	/**
